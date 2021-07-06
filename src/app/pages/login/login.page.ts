4a2cd28c56a0bd2826a8b13687b7c19f01e3e5de
Susana Santos Moreno <susanasantosmoreno97@gmail.com>
2021-07-06 11:37:38 +0200

    login and signup

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { IonSlides } from '@ionic/angular';
+import { AlertController, IonSlides, NavController } from '@ionic/angular';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -9,6 +9,8 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+
+  @ViewChild('slide_principal') slides: IonSlides;
 
   avatars = [
     {
@@ -54,24 +56,59 @@ export class LoginPage implements OnInit {
   }
 
   loginUser = {
+    mail: "",
+    password: ""
+  }
+
+  signUpUser = {
     mail: '',
-    password: ''
+    password: '',
+    name: '',
+    role: 'USER_ROLE'
   }
 
-  @ViewChild('slide_principal') slides: IonSlides;
+  constructor(private userService: UserService, private navCtrl: NavController, private alertCtrl: AlertController) { }
 
-  constructor(private userService: UserService) { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  async presentAlert(message: string) {
+    const alert = await this.alertCtrl.create({
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
-  login(formLogin: NgForm) {
-    console.log(formLogin.valid);
-    console.log(this.loginUser);
+  async login(formLogin: NgForm) {
+
+    if (formLogin.invalid) { return; }
+
+    const valid = await this.userService.login(this.loginUser.mail, this.loginUser.password);
+
+    console.log(valid);
+    if (valid) {
+      this.navCtrl.navigateRoot('/main/tabs/tab1', { animated: true });
+    } else {
+      await this.presentAlert('Email or password incorrect');
+    }
   }
 
-  signup(formSignUp: NgForm) {
-    console.log(formSignUp.valid)
+  async signup(formSignUp: NgForm) {
+    if (formSignUp.invalid) { return; }
+
+    const valid = await this.userService.signup(this.signUpUser);
+
+    if (valid) {
+      this.navCtrl.navigateRoot('/main/tabs/tab1', { animated: true });
+    } else {
+      await this.presentAlert('Data incorrect');
+    }
+
+    console.log(valid);
   }
 
   seleccionarAvatar(avatar) {
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Storage } from "@capacitor/storage";
+import { User } from '../Interfaces/interfaces';
 
 const url_users = environment.url_users;
 
@@ -13,10 +15,45 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  login(email: string, password: string) {
-    const data = { email, password };
-    this.http.post(`${url_users}/api/user/login`, data).subscribe(resp => {
-      console.log(resp);
+  login(mail: string, password: string) {
+    const data = { mail, password };
+
+    return new Promise(resolve => {
+      this.http.post(`${url_users}/api/auth/login`, data).subscribe(resp => {
+
+        if (resp['msg'] === 'Login ok') {
+          this.token = resp['token'];
+          this.saveToken(this.token);
+          resolve(true);
+        } else {
+          this.token = null;
+          Storage.clear();
+          resolve(false);
+        }
+      });
+    });
+  }
+
+  async saveToken(token: string) {
+    this.token = token;
+    Storage.set({
+      key: 'token',
+      value: JSON.stringify(this.token)
+    });
+  }
+
+  signup(user: User) {
+    return new Promise(resolve => {
+      this.http.post(`${url_users}/api/user`, user).subscribe({
+        next: resp => {
+          console.log(resp);
+          resolve(true);
+        },
+        error: error => {
+          console.log(error);
+          resolve(false);
+        }
+      });
     });
   }
 }
